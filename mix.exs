c92b653ed53357d8efd47958b18bfc312669dd52
Brendan Carpio <bfcarpio@gmail.com>
2019-06-25 18:03:14 -0400

    :pushpin: add json parser

--- mix.exs
+++ mix.exs
@@ -7,7 +7,22 @@ defmodule Exnoops.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      docs: [extras: ["README.md"]],
+      description: description()
+    ]
+  end
+
+  def description, do: "Github's Noop API client for Elixir"
+
+  def package do
+    [
+      name: :exnoop,
+      maintainers: ["Brendan Carpio"],
+      licenses: ["MIT"],
+      docs: [extras: ["README.md"]],
+      links: %{"Gitlab" => "https://gitlab.com/bfcarpio/exnoops"}
     ]
   end
 
@@ -23,7 +38,8 @@ defmodule Exnoops.MixProject do
     [
       {:earmark, "~> 1.3.2", only: :dev},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
-      {:mojito, "~> 0.3.0"}
+      {:mojito, "~> 0.3.0"},
+      {:json, "~> 1.3.0"}
     ]
   end
 end
