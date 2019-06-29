5c771149b0ab7cdf425bcd09f9219351bea59aa5
Brendan Carpio <bfcarpio@gmail.com>
2019-06-29 00:01:24 -0400

    :pencil: exnoop & get_color() docs

--- lib/exnoops.ex
+++ lib/exnoops.ex
@@ -1,18 +1,13 @@
 defmodule Exnoops do
   @moduledoc """
-  Documentation for Exnoops.
+  Elixir API wrapper around [Github's Noops](https://noopschallenge.com/)
+
+  **Note:** This wrapper was made for educational purposes; ie learning elixir and making libraries with it. This libraries probably isn't of the quality that you'd want for production use.
+
+  Pull requests welcome as long as they contain explanations as to why something should be changed (once again, to faciltate learning)
+
+  ---
+
+  Each noop is represented by a module and each endpoint is represented by a function.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Exnoops.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- lib/exnoops/hexbot.ex
+++ lib/exnoops/hexbot.ex
@@ -1,6 +1,6 @@
 defmodule Exnoops.Hexbot do
   @moduledoc """
-  Module to itneract with Github's Noop: Hexbot
+  Module to interact with Github's Noop: Hexbot
   """
 
   require Logger
@@ -8,6 +8,20 @@ defmodule Exnoops.Hexbot do
 
   @noop "hexbot"
 
+  @doc """
+  Query Hexbot for color(s)
+
+  See the [official `noop` documentation](https://noopschallenge.com/challenges/hexbot) for the most information on the bot
+
+  Returns a tuple with `{:ok, response}` or `{:error, message}`
+
+  ## Examples
+
+      iex> Exnoops.Hexbot.get_color()
+      {:ok, [%{"value" => "#18453B"}]}
+
+  """
+  @spec get_color(keyword()) :: {atom(), list()}
   def get_color(opts \\ []) do
     Logger.debug("Calling Hexbot.get_color(#{opts})")
 
