19574a3a3877c65d81803d793ad4708b50d3a8a7
Brendan Carpio <bfcarpio@gmail.com>
2019-07-19 22:17:16 -0400

    :sparkles: sortbot

--- lib/exnoops/http_mock.ex
+++ lib/exnoops/http_mock.ex
@@ -429,6 +429,11 @@ defmodule Exnoops.API.HttpMock do
           "elapsed": 1928,
           "completed": "2019-06-24T22:11:39.231Z"
         }>)
+
+      "/sortbot" ->
+        mock_mojito(200, ~S<{
+          "message": "Hello there, welcome to the Sortbot. Get started at /sortbot/exam"
+        }>)
     end
   end
 
@@ -530,6 +535,25 @@ defmodule Exnoops.API.HttpMock do
           "mazeExitDistance": 6,
           "locationPath": "/pathbot/rooms/LU62ZaD_SqudPvH3Qt3kJQ"
         }>)
+
+      {"/sortbot/exam/3qm7iTRNtpn8NIUqtBubvQ", "{\"solution\":[32,35,40,46,52,53,73,80,95,96]}"} ->
+        mock_mojito(200, ~S<{
+          "result": "success",
+          "message": "Great job!",
+          "elapsed": 0,
+          "nextSet": "/sortbot/exam/6YXxoDcJDtzp8Aj3iY0W0A"
+        }>)
+
+      {"/sortbot/exam/start", "{\"login\":\"test\"}"} ->
+        mock_mojito(200, ~S<{
+          "message": "Hello test, get ready for all sorts of fun. Get it?",
+          "nextSet": "/sortbot/exam/3qm7iTRNtpn8NIUqtBubvQ"
+        }>)
+
+      {"/sortbot/NotAPath", "{\"error\":\"...\"}"} ->
+        mock_mojito(404, ~S<mock_mojito(404, ~S/{
+          "message": "The requested resource was not found"
+        }/)>)
     end
   end
 end
---
+++ lib/exnoops/sortbot.ex
@@ -0,0 +1,62 @@
+defmodule Exnoops.Sortbot do
+  @moduledoc """
+  Module to interact with Github's Noop: Sortbot
+
+  See the [official `noop` documentation](https://noopschallenge.com/challenges/sortbot) for API information including the accepted parameters.
+  """
+
+  require Logger
+  import Exnoops.API
+
+  @noop "sortbot"
+
+  @doc """
+  Query Sortbot for welcome
+
+  ## Examples
+
+      iex> Exnoops.Sortbot.welcome()
+      {:ok, "Hello there, welcome to the Sortbot. Get started at /sortbot/exam"}
+  """
+  @spec welcome() :: {atom(), String.t()}
+  def welcome() do
+    Logger.debug("Calling Sortbot.welcome()")
+
+    case get("/" <> @noop, []) do
+      {:ok, %{"message" => value}} -> {:ok, value}
+      error -> error
+    end
+  end
+
+  @doc """
+  Submit responses to Sortbot
+
+  ## Examples
+
+      iex> Exnoops.Sortbot.submit("/sortbot/exam/start", login: "test")
+      {:ok, %{
+        "message" => "Hello test, get ready for all sorts of fun. Get it?",
+        "nextSet" => "/sortbot/exam/3qm7iTRNtpn8NIUqtBubvQ"
+      }}
+
+      iex> Exnoops.Sortbot.submit("/sortbot/exam/3qm7iTRNtpn8NIUqtBubvQ", solution: [32, 35, 40, 46, 52, 53, 73, 80, 95, 96])
+      {:ok, %{
+        "result" => "success",
+        "message" => "Great job!",
+        "elapsed" => 0,
+        "nextSet" => "/sortbot/exam/6YXxoDcJDtzp8Aj3iY0W0A"
+      }}
+  """
+  @spec submit(String.t(), keyword()) :: {atom(), map()}
+  def submit(path, body) when is_binary(path) and is_list(body) do
+    Logger.debug("Calling Sortbot.submit()")
+
+    case post(path, body) do
+      {:ok, _} = res ->
+        res
+
+      error ->
+        error
+    end
+  end
+end
